fafcf8037a3d82ab0e10f4b32a57b625c577d8e6
Prayag15-bit <64478518+Prayag15-bit@users.noreply.github.com>
2020-11-17 22:13:03 +0530

    Update LVM.py

--- LVM.py
+++ LVM.py
@@ -2,43 +2,42 @@ import os
 
 
 while True :
-
-	os.system("clear")
-
-	print("""
-	press 1: to make LVM partition
-	press 2: to exit
-	""")
-
-	ch = input("enter your choice : ")
-
-	if ch == "1":
-		pv1 = input("enter name/path of the disk 1 : ")
-		pv2 = input("enter name/path of the disk 2 : ")
-		os.system("pvcreate /dev/{}".format(pv1))
-		os.system("pvcreate /dev/{}".format(pv2))
-		vgn = input("enter name for your vg : ")
-		os.system("vgcreate {} /dev/{} /dev/{}".format(vgn,pv1,pv2))
-		lvsize = input("enter size of your lv : ")
-		lvname = input("enter name of your lv : ")
-		os.system("lvcreate --size +{} --name {} {}".format(lvsize,lvname,vgn))
-		os.system("tput setaf 3")		
-		print("successfully createed logical volume..")
-		os.system("tput setaf 7")
-		os.system("lvdisplay /dev/{}/{}".format(vgn,lvname))
-		input("press enter to format your lv {}".format(lvname))
-		os.system("mkfs.ext4 /dev/{}/{}".format(vgn,lvname))
-		print("succesfully formatted you lv {}".format(lvname))
-		input("press enter to make folder where u want to mount your lv")
-		fol = input("enter name for your folder : ")
-		os.system("mkdir /{}".format(fol))
-		print("mounting your lv {} to the /{} folder".format(lvname,fol))
-		os.system("mount /dev/{}/{}  /{}".format(vgn,lvname,fol))
-		print("successfully mounted ur lv {} to /{} folder".format(lvname,fol))
-	
-	elif ch == "2":
-		exit()
-
-	else :
-		print("Invalid choice")
-	input()
+    os.system("clear")
+    print("------------------------------------------------")
+    print("                       ___          _____  _____")
+    print("|   \    / |\  /|     |   |   /\   |      |     ")
+    print("|    \  /  | \/ | --- |___|  /--\  |  --| |---  ")
+    print("|___  \/   |    |     |     /    \ |____| |_____")
+    print("------------------------------------------------")
+    print("\n")
+    print("""
+    press 1: to make LVM partition
+    press 2: to exit
+    """)
+    ch = input("enter your choice : ")
+    if ch == "1":
+        pv1 = input("enter name/path of the disk 1 : ")
+        os.system("pvcreate /dev/{}".format(pv1))
+        vgn = input("enter name for your vg : ")
+        os.system("vgcreate {} /dev/{} /dev/{}".format(vgn,pv1))
+        lvsize = input("enter size of your lv : ")
+        lvname = input("enter name of your lv : ")
+        os.system("lvcreate --size +{} --name {} {}".format(lvsize,lvname,vgn))
+        os.system("tput setaf 3")		
+        print("successfully createed logical volume..")
+        os.system("tput setaf 7")
+        os.system("lvdisplay /dev/{}/{}".format(vgn,lvname))
+        input("press enter to format your lv {}".format(lvname))
+        os.system("mkfs.ext4 /dev/{}/{}".format(vgn,lvname))
+        print("succesfully formatted you lv {}".format(lvname))
+        input("press enter to make folder where u want to mount your lv")
+        fol = input("enter name for your folder : ")
+        os.system("mkdir /{}".format(fol))
+        print("mounting your lv {} to the /{} folder".format(lvname,fol))
+        os.system("mount /dev/{}/{}  /{}".format(vgn,lvname,fol))
+        print("successfully mounted ur lv {} to /{} folder".format(lvname,fol))
+    elif ch == "2":
+        exit()
+    else :
+        print("Invalid choice")
+        input()
